e1f6d28171d4440c13cea4152b49e1545b560480
leofnh <leofnh@live.com>
2025-09-14 23:24:21 -0300

    update: kids

--- appstore/settings.py
+++ appstore/settings.py
@@ -54,25 +54,25 @@ WSGI_APPLICATION = 'appstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 
 #postgres
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ael',
-#         'USER': 'admin',
-#         'PASSWORD': 'leonardo01',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-#  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ael',
+        'USER': 'admin',
+        'PASSWORD': 'leonardo01',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
